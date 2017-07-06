f574b71c89430a596d149af2e49be66e88cd69ee
Rory <srsullivan01@gmail.com>
2017-07-06 19:05:28 -0400

    completed countdown, bar, fuse, and crawler and updated colors

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -2,17 +2,29 @@
 // Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
 // See LICENSE for details.
 
+//need to make timer, tell timer to count down, display that countdown
+
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+    var numericDisplay = document.getElementById('numeric-display').innerHTML = timerValue;
   },
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    var timeElapsed = 100 - timerValue;
+    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
   },
   drawLitFuses: function(timerValue){
-    // Your Code Here
+    var percentUnburnt = timerValue/100;
+    document.getElementsByClassName('unburnt')[0].style.width = percentUnburnt*98 + '%';
+    document.getElementsByClassName('burnt')[0].style.width = (1 - percentUnburnt)*98 + '%';
   },
   drawCrawlers: function(timerValue){
-    // Your Code Here
+    var timeElapsed = 100 - timerValue;
+    if (timerValue%2 === 0) {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '0px';
+    }
+    else {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '10px';
+    }
+    document.getElementsByClassName('crawler')[0].style.marginLeft = (timeElapsed*10) + 'px';
   }
 };
