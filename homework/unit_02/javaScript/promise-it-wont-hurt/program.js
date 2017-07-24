c66d905a41388603032efff450e9258d56ec788d
Rory <srsullivan01@gmail.com>
2017-07-24 19:56:49 -0400

    fifth exercise

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var promise = new Promise(function (fulfill, reject) {
-    fulfill('I FIRED');
-    reject(new Error('I DID NOT FIRE'));
+    fulfill('PROMISE VALUE');
 });
-function onReject(error) {
-  console.log(error.message);
-}
-promise.then(console.log, onReject);
+
+promise.then(console.log);
+console.log('MAIN PROGRAM');
