48b2667bd32d072f37db6c69be2414cfad351378
Rory <srsullivan01@gmail.com>
2017-07-24 19:45:29 -0400

    second excercise

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -1,3 +1,9 @@
-setTimeout(function(){
-  console.log("TIMED OUT!");
-}, 300);
+var promise = new Promise(function (fulfill, reject) {
+  // After the timeout reaches 300ms, fulfill the promise with value
+  // 'FULFILLED!'.
+
+  setTimeout(function () {
+    fulfill('FULFILLED!');
+  }, 300);
+});
+promise.then(console.log);
