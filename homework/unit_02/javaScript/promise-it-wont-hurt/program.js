f1aed25e0b7bb99dbe9b5aa4f069e6fde4970fe1
Rory <srsullivan01@gmail.com>
2017-07-24 20:05:15 -0400

    sixth exercise

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -1,8 +1,29 @@
 'use strict';
 
-var promise = new Promise(function (fulfill, reject) {
-    fulfill('PROMISE VALUE');
-});
+var message;
+var promise;
 
-promise.then(console.log);
-console.log('MAIN PROGRAM');
+function randomBytes(n) {
+  return (Math.random() * Math.pow(32, n) | 0).toString(19);
+}
+
+message =
+  'A fatal exception ' + randomBytes(1) + ' has occurred at ' +
+  randomBytes(2) + ':' + randomBytes(4) + '. Your system will explode in 3 seconds.';
+
+promise = Promise.reject(new Error(message));
+
+promise.catch(function (err) {
+  var i = 3;
+
+  process.stderr.write(err.message);
+
+  setTimeout(function boom() {
+    process.stderr.write('\rwill be terminated in ' + (--i) + ' seconds.');
+    if (!i) {
+      process.stderr.write('\n..... . . . boom . . . .....\n');
+    } else {
+      setTimeout(boom, 1000);
+    }
+  }, 1000);
+});
